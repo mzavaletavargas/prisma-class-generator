d7da0413d82a62280b26732f7784b2b56d009c7c
Gustavo Zavaleta <mzavaletavargas@gmail.com>
2023-10-13 14:10:27 -0500

    testing type

--- src/components/file.component.ts
+++ src/components/file.component.ts
@@ -101,11 +101,11 @@ export class FileComponent implements Echoable {
 			})
 		})
 
-		if (this.prismaClass.types) {
-			this.prismaClass.types.forEach(type => {
-				this.registerImport(type + "Type", "./" + type.toLowerCase() + "_type")
-			})
-		}
+		// if (this.prismaClass.types) {
+		// 	this.prismaClass.types.forEach(type => {
+		// 		this.registerImport(type + "Type", "./" + type.toLowerCase() + "_type")
+		// 	})
+		// }
 
 		if (generator.getConfig().useGraphQL) {
 			this.registerImport('ID', '@nestjs/graphql')
